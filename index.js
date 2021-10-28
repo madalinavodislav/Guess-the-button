8d1bb689bf8e50ec15cd093f762ff7d1f2c8069e
Madalina Vodislav <81998781+madalinavodislav@users.noreply.github.com>
2021-10-29 00:53:42 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+function theButton(id) {
+  var winner = Math.floor(Math.random() * 3) + 1;
+  if (id == winner) {
+    alert("Congratulations! Your choice was the right one!");
+    var property = document.getElementById(id);
+    document.getElementById("img1").style.visibility = "visible";
+    document.getElementById("img2").style.visibility = "hidden";
+    property.style.backgroundColor = "green";
+    return true;
+  } else {
+    alert("Try again!");
+    var property = document.getElementById(id);
+    document.getElementById("img2").style.visibility = "visible";
+    document.getElementById("img1").style.visibility = "hidden";
+    property.style.backgroundColor = "red";
+    return false;
+  }
+}
+function refreshPage(){
+  window.location.reload();
+}
